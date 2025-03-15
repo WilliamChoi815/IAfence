59b17f91a9af70a76fb9d54c2470a88a7aa70c28
WilliamChoi815 <william.hs.choi@gmail.com>
2025-03-15 22:22:30 +0900

    Add Player, Tournament, and related models with admin registration

--- backend/fencingproject/website/admin.py
+++ backend/fencingproject/website/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Match)
+admin.site.register(models.Player)
+admin.site.register(models.Tournament)
+admin.site.register(models.Tournament_Bracket)
+admin.site.register(models.Player_Point)
+admin.site.register(models.Tournament_Player)
+
+
 
--- backend/fencingproject/website/models.py
+++ backend/fencingproject/website/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Tournament(models.Model):
+    EVENT_TYPE_CHOICES = [
+        ("Sabre", "Sabre"),
+        ("Foil", "Foil"),
+        ("Epee", "Epee"),
+    ]
+     
+    tournament_id = models.AutoField(primary_key=True)
+    event_type = models.CharField(max_length=50, choices = EVENT_TYPE_CHOICES)
+    tournament_name = models.CharField(max_length=100)
+    tournament_date = match_date = models.DateTimeField()
+    completed = models.BooleanField() 
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Match(models.Model):
     MATCH_TYPE_CHOICES = [ 
         ("Group Stage", "Group Stage"),
@@ -20,13 +36,13 @@ class Match(models.Model):
     ]
 
     match_id = models.AutoField(primary_key=True)
-    #tournament_id = models.ForeignKey("Tournament", on_delete = models.CASCADE, related_name = "matches")
+    tournament_id = models.ForeignKey("Tournament", on_delete = models.CASCADE, related_name = "tournaments")
     match_type = models.CharField(max_length=50, choices= MATCH_TYPE_CHOICES)
     event_type = models.CharField(max_length=50, choices = EVENT_TYPE_CHOICES)
     match_date = models.DateTimeField()
-    #player1 = models.ForeignKey("Player", related_name = "matches_as_player1")
+    player1 = models.ForeignKey("Player", related_name = "matches_as_player1")
     player1_score = models.IntegerField()
-    #player2 = models.ForeignKey("Player", related_name = "matches_as_player2")
+    player2 = models.ForeignKey("Player", related_name = "matches_as_player2")
     player2_score = models.IntegerField()
     player1_yellowcard = models.IntegerField()
     player2_yellowcard = models.IntegerField()
@@ -35,4 +51,44 @@ class Match(models.Model):
     completed = models.BooleanField()
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Player(models.Model):
+    player_id = models.AutoField(primary_key=True)
+    player_name = models.CharField(max_length=100)
+    player_age = models.IntegerField()
+    player_affiliation = models.CharField(max_length=100)   
+    profile_link = models.TextField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tournament_Bracket(models.Model):
+    EVENT_TYPE_CHOICES = [
+        ("Sabre", "Sabre"),
+        ("Foil", "Foil"),
+        ("Epee", "Epee"),
+    ]
+
+    tournament_bracket_id = models.AutoField(primary_key=True)
+    tournament_id = models.ForeignKey("Tournament", on_delete = models.CASCADE, related_name = "matches")
+    event_type = models.CharField(max_length=50, choices = EVENT_TYPE_CHOICES)
+    match_id = models.ForeignKey( "Match", on_delete = models.CASCADE, related_name = "matches")
+    match_number = models.IntegerField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Player_Point(models.Model):
+    player_points_id = models.AutoField(primary_key=True)
+    player_id = models.ForeignKey("Players", on_delete = models.CASCADE, related_name = "player_points")
+    match_id = models.ForeignKey( "Match", on_delete = models.CASCADE, related_name = "matches")
+    match_date = models.DateTimeField()
+    points_earned = models.IntegerField()
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tournament_Player(models.Model):
+    tournament_player_id = models.AutoField(primary_key=True)
+    tournament_id = models.ForeignKey("Tournament", on_delete = models.CASCADE, related_name = "tournaments")
+    player_id = models.ForeignKey("Player", on_delete = models.CASCADE, related_name = "players")
+    
+
     
