a458d160b63f94ad21f9c2978be0c418f4ed7b29
WilliamChoi815 <william.hs.choi@gmail.com>
2025-03-15 22:22:22 +0900

    Add API views and URL routes for Match model with test response

--- backend/fencingproject/website/urls.py
+++ backend/fencingproject/website/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('sample/', views.sample_api.as_view())
+    path('test/', views.Test.as_view(), name = "test"),
+    path('matches/', views.MatchList.as_view(), name = "match-list"),
+    path('mathces/<int:pk>/', views.MatchDetail.as_view(), name = "match-detail")
 ]
--- backend/fencingproject/website/views.py
+++ backend/fencingproject/website/views.py
@@ -1,16 +1,62 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Match
+from .serializers import MatchSerializer
 
-class sample_api(APIView):
+# This is a test response
+class Test(APIView):
     def get(self, request):
         data = {
-            'message': 'Hello, this is a sample API response!'
+            'message': 'Hello, this is a test response!'
         }
         return Response(data)
+
+# Get all matches and create new match
+class MatchList(APIView):
+    def get(self, request):
+        matches = Match.objects.all()
+        serializer = MatchSerializer(matches, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MatchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Get, update, or delete a match
+class MatchDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Match.objects.get(pk=pk)
+        except Match.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        match = self.get_object(pk)
+        serializer = MatchSerializer(match)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        match = self.get_object(pk)
+        serializer = MatchSerializer(match, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        match = self.get_object(pk)
+        match.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+    
+    
 
 
